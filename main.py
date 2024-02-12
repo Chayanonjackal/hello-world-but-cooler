6e8f0ba8f1890022cde5977d5dfb7c54f7c1d074
FROSSTY <aom_jackal@hotmail.com>
2024-02-13 03:23:08 +0800

    udpate main startpoint

--- main.py
+++ main.py
@@ -2,39 +2,53 @@ import random
 import string
 import time
 
+
 def generate_random_text(length):
-    characters = string.ascii_letters + string.digits + string.punctuation + ' '
-    return ''.join(random.choice(characters) for _ in range(length))
-    
-def generate_hello_world(words):
+    characters = string.ascii_letters + string.digits + string.punctuation + " "
+    return "".join(random.choice(characters) for _ in range(length))
+
+
+def generate_words(words):
     correct_word = ""
     for i in words:
-      right_word = ""
-      while right_word != i:
-        time.sleep(0.01)
-        right_word = generate_random_text(1)
-        print(correct_word + right_word)
-        if right_word == i:
-          print(correct_word)
-          correct_word += right_word
+        right_word = ""
+        while right_word != i:
+            time.sleep(0.01)
+            right_word = generate_random_text(1)
+            print(correct_word + right_word)
+            if right_word == i:
+                correct_word += right_word
     return correct_word
 
+
 def get_ascii_letter_input(prompt):
-   while True:
-      user_input = input(prompt)
-      if is_ascii_letter(user_input):
-         return user_input
-      else:
-         print("Please enter only ASCII letters.")
+    while True:
+        user_input = input(prompt)
+        if is_ascii_letter(user_input):
+            return user_input
+        else:
+            print("Please enter only ASCII letters.")
+
 
 def is_ascii_letter(input_string):
-    for char  in input_string:
-        if not('a' <= char <= 'z' or 'A' <= char <= 'Z' or '0' <= char <= '9' or is_special_character(char)):
-           return False
+    for char in input_string:
+        if not (
+            "a" <= char <= "z"
+            or "A" <= char <= "Z"
+            or "0" <= char <= "9"
+            or is_special_character(char)
+        ):
+            return False
     return True
 
-def is_special_character(char):
-   return not char.isalnum()
 
-user_input = get_ascii_letter_input("Enter ASCII letters only: ")
-print(generate_hello_world(user_input))
+def is_special_character(char):
+    return not char.isalnum()
+
+
+def main():
+    print("output : ",generate_words(get_ascii_letter_input("Enter ASCII letters only: ")))
+
+
+if __name__ == "__main__":
+    main()
